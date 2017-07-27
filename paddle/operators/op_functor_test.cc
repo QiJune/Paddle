2d1ba5de48ea119f5f6b630407031371cafb509d
qijun <qijun1994@hotmail.com>
2017-07-27 11:35:21 +0800

    add mul op functor gpu test

--- paddle/operators/op_functor_test.cc
+++ paddle/operators/op_functor_test.cc
@@ -44,9 +44,9 @@ TEST(OpFunctor, AddCPU) {
   EXPECT_EQ(t_c[3], 6);
 }
 
-TEST(OpFunctor, Mul) { EXPECT_EQ(1, 1); }
+TEST(OpFunctor, MulCPU) { EXPECT_EQ(1, 1); }
 
-TEST(OpFunctor, Softmax) { EXPECT_EQ(1, 1); }
+TEST(OpFunctor, SoftmaxCPU) { EXPECT_EQ(1, 1); }
 
 #ifndef PADDLE_ONLY_CPU
 TEST(OpFunctor, AddGPU) {
@@ -87,5 +87,45 @@ TEST(OpFunctor, AddGPU) {
   EXPECT_EQ(t_c[1], 2);
   EXPECT_EQ(t_c[2], 4);
   EXPECT_EQ(t_c[3], 6);
+}
+
+TEST(OpFunctor, MulGPU) {
+  int size = 4;
+
+  float* t_a = (float*)malloc(size * sizeof(float));
+  float* t_b = (float*)malloc(size * sizeof(float));
+  float* t_c = (float*)malloc(size * sizeof(float));
+  for (int i = 0; i < size; i++) {
+    t_a[i] = i;
+    t_b[i] = i;
+  }
+
+  Tensor t1;
+  t1.mutable_data<float>({2, 2}, GPUPlace(0));
+
+  Tensor t2;
+  t2.mutable_data<float>({2, 2}, GPUPlace(0));
+
+  Tensor t3;
+  t3.mutable_data<float>({2, 2}, GPUPlace(0));
+
+  cudaMemcpy(
+      t1.data<float>(), t_a, size * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(
+      t2.data<float>(), t_b, size * sizeof(float), cudaMemcpyHostToDevice);
+
+  functors::mul<GPUPlace, float> functor;
+
+  DeviceContext* device = new CUDADeviceContext(0);
+
+  functor(*device, t1, t2, &t3);
+
+  cudaMemcpy(
+      t_c, t3.data<float>(), size * sizeof(float), cudaMemcpyDeviceToHost);
+
+  EXPECT_EQ(t_c[0], 2);
+  EXPECT_EQ(t_c[1], 3);
+  EXPECT_EQ(t_c[2], 6);
+  EXPECT_EQ(t_c[3], 11);
 }
 #endif
